471b83e4726ca7a7a4306e9bd9a03b2adc389b94
Ugur Guney <ugurguney@gmail.com>
2025-06-13 01:06:18 -0400

    FORWARD_DEFINE_VK_HANDLE and `VK_NULL_HANDLE nullptr` to Utils.h

--- src/Utils.h
+++ src/Utils.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#define FORWARD_DEFINE_VK_HANDLE(object) typedef struct object##_T* object;
+#if !defined(VK_NULL_HANDLE)
+#define VK_NULL_HANDLE nullptr
+#endif
+
 namespace aur {
 
 // Signed integers
