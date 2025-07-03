138dcf1ec99aeed7fda96814814e295e854cd43c
Ugur Guney <ugurguney@gmail.com>
2025-07-02 20:38:12 -0400

    slightly improve render::Mesh

--- src/render/Mesh.h
+++ src/render/Mesh.h
@@ -3,13 +3,28 @@
 #include <string>
 
 #include "../Resources/Buffer.h"
+#include "../asset/Handle.h"
+
+namespace aur::asset {
+struct Mesh;
+}
 
 namespace aur::render {
+
+struct DrawSpan {
+  // TODO(vug): bring render::Material
+  // Handle<render::Material> material;
+  u32 offset;
+  u32 count;
+};
 
 struct Mesh {
   Buffer vertexBuffer;
   Buffer indexBuffer;
   std::string debugName;
+
+  std::vector<DrawSpan> drawSpans;
+  Handle<asset::Mesh> asset;
 };
 
 } // namespace aur::render
