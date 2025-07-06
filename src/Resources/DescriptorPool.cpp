74f1cfa92bbad7d6c0a07957c2448d348868aade
Ugur Guney <ugurguney@gmail.com>
2025-07-06 00:14:39 -0400

    Migrate DescriptorPool to VulkanResource format too.

--- src/Resources/DescriptorPool.cpp
+++ src/Resources/DescriptorPool.cpp
@@ -4,64 +4,42 @@
 
 #include "../Logger.h"
 
-#include <VkBootstrap.h>
-
 namespace aur {
-DescriptorPool::DescriptorPool(VkDevice device, const DescriptorPoolCreateInfo& poolCreateInfo)
-    : device_{device}
-    , createInfo_{poolCreateInfo}
-    , handle_{[this, device, &poolCreateInfo]() {
-      std::vector<VkDescriptorPoolSize> vkPoolSizes;
-      vkPoolSizes.reserve(createInfo_.poolSizes.size());
-      for (const auto& size : createInfo_.poolSizes) {
-        vkPoolSizes.push_back({
-            .type = static_cast<VkDescriptorType>(size.type),
-            .descriptorCount = size.count,
-        });
-      }
-      const VkDescriptorPoolCreateInfo descPoolInfo{
-          .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO,
-          .flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT,
-          .maxSets = 512,
-          .poolSizeCount = static_cast<u32>(vkPoolSizes.size()),
-          .pPoolSizes = vkPoolSizes.data(),
-      };
 
-      VkDescriptorPool hnd{};
-      VK(vkCreateDescriptorPool(device, &descPoolInfo, nullptr, &hnd));
-      return hnd;
-    }()} {}
+DescriptorPool::DescriptorPool(VkDevice device, const DescriptorPoolCreateInfo& createInfo)
+    : VulkanResource{createInfo, device} {}
 
 DescriptorPool::~DescriptorPool() {
   destroy();
 }
 
-DescriptorPool::DescriptorPool(DescriptorPool&& other) noexcept
-    : device_{std::exchange(other.device_, {})}
-    , createInfo_{std::exchange(other.createInfo_, {})}
-    , handle_{std::exchange(other.handle_, {})} {}
+VkDescriptorPool DescriptorPool::createImpl(const DescriptorPoolCreateInfo& createInfo,
+                                            const std::tuple<VkDevice>& context) {
+  std::vector<VkDescriptorPoolSize> vkPoolSizes;
+  vkPoolSizes.reserve(createInfo.poolSizes.size());
+  for (const auto& size : createInfo.poolSizes) {
+    vkPoolSizes.push_back({
+        .type = static_cast<VkDescriptorType>(size.type),
+        .descriptorCount = size.count,
+    });
+  }
 
-DescriptorPool& DescriptorPool::operator=(DescriptorPool&& other) noexcept {
-  if (this == &other)
-    return *this;
+  const VkDescriptorPoolCreateInfo vkCreateInfo{
+      .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO,
+      .flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT,
+      .maxSets = createInfo.maxSets,
+      .poolSizeCount = static_cast<u32>(vkPoolSizes.size()),
+      .pPoolSizes = vkPoolSizes.data(),
+  };
 
-  destroy();
+  VkDescriptorPool hnd{VK_NULL_HANDLE};
+  VK(vkCreateDescriptorPool(std::get<VkDevice>(context), &vkCreateInfo, nullptr, &hnd));
 
-  device_ = std::exchange(other.device_, {});
-  createInfo_ = std::exchange(other.createInfo_, {});
-  handle_ = std::exchange(other.handle_, {});
-  return *this;
+  return hnd;
 }
 
-void DescriptorPool::invalidate() {
-  handle_ = VK_NULL_HANDLE;
+void DescriptorPool::destroyImpl() const {
+  vkDestroyDescriptorPool(std::get<VkDevice>(context_), handle_, nullptr);
 }
 
-void DescriptorPool::destroy() {
-  if (!isValid())
-    return;
-
-  vkDestroyDescriptorPool(device_, handle_, nullptr);
-  invalidate();
-}
 } // namespace aur
--- src/Resources/DescriptorPool.h
+++ src/Resources/DescriptorPool.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../VulkanWrappers.h"
+#include "VulkanResource.h"
 
 namespace aur {
 
@@ -14,28 +15,22 @@ struct DescriptorPoolCreateInfo {
   std::vector<DescriptorPoolSize> poolSizes;
 };
 
-class DescriptorPool {
+class DescriptorPool
+    : public VulkanResource<DescriptorPool, VkDescriptorPool, DescriptorPoolCreateInfo, VkDevice> {
 public:
   DescriptorPool() = default;
   DescriptorPool(VkDevice device, const DescriptorPoolCreateInfo& createInfo);
   ~DescriptorPool();
 
-  DescriptorPool(const DescriptorPool&) = delete;
-  DescriptorPool& operator=(const DescriptorPool&) = delete;
-  DescriptorPool(DescriptorPool&& other) noexcept;
-  DescriptorPool& operator=(DescriptorPool&& other) noexcept;
-
-  [[nodiscard]] inline const DescriptorPoolCreateInfo& getCreateInfo() const { return createInfo_; }
-  [[nodiscard]] inline const VkDescriptorPool& getHandle() const { return handle_; }
-  [[nodiscard]] inline bool isValid() const { return handle_ != VK_NULL_HANDLE; }
+  DescriptorPool(DescriptorPool&& other) noexcept = default;
+  DescriptorPool& operator=(DescriptorPool&& other) noexcept = default;
 
 private:
-  void invalidate();
-  void destroy();
+  friend class VulkanResource<DescriptorPool, VkDescriptorPool, DescriptorPoolCreateInfo, VkDevice>;
 
-  VkDevice device_{VK_NULL_HANDLE};
-  DescriptorPoolCreateInfo createInfo_;
-  VkDescriptorPool handle_{VK_NULL_HANDLE};
+  static VkDescriptorPool createImpl(const DescriptorPoolCreateInfo& createInfo,
+                                     const std::tuple<VkDevice>& context);
+  void destroyImpl() const;
 };
 
 } // namespace aur
