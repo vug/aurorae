6e2ebea65b182ed872f00283dfd01598a4602312
Ugur Guney <ugurguney@gmail.com>
2025-08-08 09:50:32 -0400

    todo

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 /*
+TODO(vug): make new enums convertable to string (locality)
 TODO(vug): Use new depth parameters, and blending presets in building vulkan create into.
 TODO(vug): render::Material uses the hash of PipelineCreateInfo to load Pipeline if cached on disk
            otherwise create one and store to disk (do storing operations on a separate thread)
